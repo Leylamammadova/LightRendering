1c25185882958058b77ab1f3391d78f7dfe4baf1
Abdullah Mukhtar Mansur Bin Abdullah <yuureijin@gmail.com>
2017-05-20 12:20:12 +0100

    Ambient lighting calculations added to plane fragment shader.

--- octet/src/examples/example_waves/shaders/plane.fs
+++ octet/src/examples/example_waves/shaders/plane.fs
@@ -4,5 +4,9 @@ uniform vec3 object_colour_;
 uniform vec3 light_colour_;
 
 void main() {
-    gl_FragColor = vec4(light_colour_ * object_colour_, 1.0);
+    float ambient_intensity = 0.1f;
+    vec3 ambient = ambient_intensity * light_colour_;
+    vec3 result = ambient * object_colour_;
+    
+    gl_FragColor = vec4(result, 1.0);
 }
